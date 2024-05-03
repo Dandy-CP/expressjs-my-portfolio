6132fb99d14f55180b21c3fb4476c7798b645c73
Dandy Candra <dandycandrapratama@gmail.com>
2024-05-03 09:07:55 +0700

    initial setup

---
+++ app.ts
@@ -0,0 +1,48 @@
+import express, { Request, Response, Application } from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import helmet from "helmet";
+import dotenv from "dotenv";
+import validateEnv from "@/utils/validateEnv";
+import MyProjectsRouter from "@/routers/my-projects.routers";
+
+dotenv.config();
+validateEnv();
+
+const app: Application = express();
+const PORT = process.env.PORT || 8080;
+const Environment = process.env.NODE_ENV;
+
+app.use(
+  cors({
+    origin: "*",
+    methods: "GET,PUT,PATCH,POST,DELETE",
+  })
+);
+app.use(helmet());
+app.use(bodyParser.json());
+
+app.get("/", (res: Response) => {
+  res.status(200).send({
+    statusCode: 200,
+    message: "Welcome To My Portfolio Rest API",
+  });
+});
+
+MyProjectsRouter(app);
+
+app.use((req: Request, res: Response) => {
+  res.status(404).json({
+    message: `Route ${req.path} Not Found`,
+    status: 404,
+  });
+});
+
+app
+  .listen(PORT, async () => {
+    console.info(`[server]: Server started on PORT ${PORT} 🚀`);
+    console.info(`[server]: Running on ${Environment} 👨‍💻👩‍💻`);
+  })
+  .on("error", (error) => {
+    console.error(`[server]: Opps Something Wrong ${error.message}`);
+  });
