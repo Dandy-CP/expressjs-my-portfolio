b07d16085eedf9df1b3f96920c1ff1b01c08d618
Dandy Candra <dandycandrapratama@gmail.com>
2024-05-13 10:15:54 +0700

    add blog type

---
+++ types/blog.types.ts
@@ -0,0 +1,10 @@
+export interface blogType {
+  id: string;
+  title: string;
+  author: string;
+  content: string;
+  thumbnail: string;
+  tag: string[];
+  createdAt: string;
+  updatedAt: string;
+}
