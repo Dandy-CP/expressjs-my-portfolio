9ce747434bcfac4f93fb3aa44adc433a602fb278
Dandy Candra <dandycandrapratama@gmail.com>
2024-05-16 10:49:40 +0700

    declare index export router

---
+++ routers/index.ts
@@ -0,0 +1,3 @@
+export { default as myProjectsRouter } from "./myprojects.routers";
+export { default as blogRouter } from "./blog.routers";
+export { default as commentBlogRouter } from "./commentBlog.routers";
