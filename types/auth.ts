da64da1671ca3cc4fa8fc7e8c322198a769497c6
Dandy Candra <dandycandrapratama@gmail.com>
2024-05-20 11:20:39 +0700

    add new type

---
+++ types/auth.ts
@@ -0,0 +1,36 @@
+export interface authResponseType {
+  id: string;
+  name?: string;
+  email?: string;
+  password?: string;
+  avatar?: string;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
+
+export interface authRegisterBodyType {
+  name: string;
+  email: string;
+  password: string;
+}
+
+export interface authLoginBodyType {
+  email: string;
+  password: string;
+}
+
+export interface changePasswordBodyType {
+  oldPassword: string;
+  newPassword: string;
+}
+
+export interface refreshTokenBodyType {
+  refreshToken: string;
+}
+
+export interface decodeTypeJWT {
+  id: string;
+  name: string;
+  iat: number;
+  exp: number;
+}
--- types/blog.types.ts
+++ types/blog.types.ts
@@ -1,10 +1,23 @@
 export interface blogType {
   id: string;
+  category: string;
   title: string;
+  content: string;
+  thumbnail: string;
+  tag: string[];
+  blogViews: number;
+  likes: number;
+  dislike: number;
+  authorId: string;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface blogBodyType {
+  title: string;
+  category: string;
   author: string;
   content: string;
   thumbnail: string;
   tag: string[];
-  createdAt: string;
-  updatedAt: string;
 }
