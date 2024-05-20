993815c9e040dedfe6670ebc6d7dff102179188f
Dandy Candra <dandycandrapratama@gmail.com>
2024-05-20 11:19:03 +0700

    enhance validator blog

--- middleware/validator/blog.validator.ts
+++ middleware/validator/blog.validator.ts
@@ -1,22 +1,69 @@
-import { body } from "express-validator";
+import { check } from "express-validator";
 
 const blogValidator = () => {
   return [
-    body("title", "title is Required").exists().notEmpty(),
-    body("title", "title must be string").isString(),
+    check("title", "title is Required").exists().notEmpty(),
+    check("title", "title must be string").isString(),
 
-    body("author", "author is Required").exists().notEmpty(),
-    body("author", "author must be string").isString(),
+    check("content", "content is Required").exists().notEmpty(),
+    check("content", "content must be string").isString(),
 
-    body("content", "content is Required").exists().notEmpty(),
-    body("content", "content must be string").isString(),
+    check("category", "category is Required").exists().notEmpty(),
+    check("category", "category must be string").isString(),
 
-    body("thumbnail", "thumbnail is Required").exists().notEmpty(),
-    body("thumbnail", "thumbnail must be string").isString(),
-    body("thumbnail", "thumbnail must be URL string").isURL(),
+    check("tag", "tag must be Array of string").custom((value, { req }) => {
+      const isArray = Array.isArray(JSON.parse(req.body.tag));
 
-    body("tag", "tag is Required").exists().notEmpty(),
-    body("tag", "tag must be Array String, Min 1").isArray({ min: 1 }),
+      if (isArray) {
+        return true;
+      } else {
+        return false;
+      }
+    }),
+
+    check("tag", "tag Array cannot be empty").custom((value, { req }) => {
+      const isArrayEmpty = JSON.parse(req.body.tag).length;
+
+      if (isArrayEmpty === 1) {
+        return true;
+      } else {
+        return false;
+      }
+    }),
+
+    check("blogThumbnail", "blogThumbnail is Required").custom(
+      (value, { req }) => {
+        const isFileExist = req.file?.buffer;
+        const path = req.path;
+
+        if (path === "/update") {
+          return true;
+        }
+
+        if (isFileExist) {
+          return true;
+        } else {
+          return false;
+        }
+      }
+    ),
+    check(
+      "blogThumbnail",
+      "blogThumbnail must be Image Type PNG/JPG/JPEG"
+    ).custom((value, { req }) => {
+      const isImageType = req.file?.mimetype.includes("image");
+      const path = req.path;
+
+      if (path === "/update") {
+        return true;
+      }
+
+      if (isImageType) {
+        return true;
+      } else {
+        return false;
+      }
+    }),
   ];
 };
 
