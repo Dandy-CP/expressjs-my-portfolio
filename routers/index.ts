c0f7499595e20fa9b848e0509c2a9adffade3736
Dandy Candra <dandycandrapratama@gmail.com>
2024-05-20 11:20:31 +0700

    add auth router

--- routers/index.ts
+++ routers/index.ts
@@ -1,3 +1,4 @@
 export { default as myProjectsRouter } from "./myprojects.routers";
 export { default as blogRouter } from "./blog.routers";
 export { default as commentBlogRouter } from "./commentBlog.routers";
+export { default as authRouter } from "./auth.routers";
