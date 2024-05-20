60045c64ab14ca6535aa700656adcf49dc0a56ee
Dandy Candra <dandycandrapratama@gmail.com>
2024-05-20 11:17:19 +0700

    enhance blog comment controller and add ability like and dislike

--- controllers/commentBlog.controllers.ts
+++ controllers/commentBlog.controllers.ts
@@ -1,5 +1,4 @@
 import { Request, Response } from "express";
-import { validationResult } from "express-validator";
 import prisma from "@/prisma/prisma";
 import { successHandler, errorHandler } from "@/middleware/responseHandler";
 import { blogCommentBodyType } from "@/types/blogComment.type";
@@ -7,21 +6,13 @@ import { blogCommentBodyType } from "@/types/blogComment.type";
 export const getBlogComment = async (req: Request, res: Response) => {
   const { id } = req.query;
 
-  const errors = validationResult(req);
-
-  if (!errors.isEmpty()) {
-    res.status(422).json({ errors: errors.array() });
-    return;
-  }
-
   try {
-    await prisma.blogComment
+    await prisma.blog_comment
       .findMany({ where: { blogId: id as string } })
       .then((data) => {
         return successHandler({
           res,
           data,
-          message: `Success Get Blog Comment with ID ${id}`,
         });
       });
   } catch (error) {
@@ -30,22 +21,15 @@ export const getBlogComment = async (req: Request, res: Response) => {
 };
 
 export const createBlogComment = async (req: Request, res: Response) => {
-  const { name, comment } = req.body as blogCommentBodyType;
+  const bodyValue = req.body as blogCommentBodyType;
   const { id } = req.query;
 
-  const errors = validationResult(req);
-
-  if (!errors.isEmpty()) {
-    res.status(422).json({ errors: errors.array() });
-    return;
-  }
-
   try {
-    const idValue = await prisma.blog.findUnique({
+    const blogInDB = await prisma.blog.findUnique({
       where: { id: id as string },
     });
 
-    if (!idValue) {
+    if (!blogInDB) {
       return errorHandler({
         res,
         customMessage: `Blog with ID ${id} not found`,
@@ -53,11 +37,10 @@ export const createBlogComment = async (req: Request, res: Response) => {
       });
     }
 
-    await prisma.blogComment
+    await prisma.blog_comment
       .create({
         data: {
-          name,
-          comment,
+          ...bodyValue,
           blog: {
             connect: {
               id: id as string,
@@ -69,7 +52,6 @@ export const createBlogComment = async (req: Request, res: Response) => {
         return successHandler({
           res,
           data,
-          message: "Success Post Blog Comment",
         });
       });
   } catch (error) {
@@ -80,19 +62,12 @@ export const createBlogComment = async (req: Request, res: Response) => {
 export const deleteCommentBlog = async (req: Request, res: Response) => {
   const { id } = req.query;
 
-  const errors = validationResult(req);
-
-  if (!errors.isEmpty()) {
-    res.status(422).json({ errors: errors.array() });
-    return;
-  }
-
   try {
-    const idValue = await prisma.blogComment.findUnique({
+    const blogInDB = await prisma.blog_comment.findUnique({
       where: { id: id as string },
     });
 
-    if (!idValue) {
+    if (!blogInDB) {
       return errorHandler({
         res,
         customMessage: `comment with ID ${id} not found`,
@@ -100,7 +75,7 @@ export const deleteCommentBlog = async (req: Request, res: Response) => {
       });
     }
 
-    await prisma.blogComment
+    await prisma.blog_comment
       .delete({ where: { id: id as string } })
       .then((data) => {
         return successHandler({
@@ -111,4 +86,64 @@ export const deleteCommentBlog = async (req: Request, res: Response) => {
   } catch (error) {
     return errorHandler({ error, res });
   }
+};
+
+export const likeBlogComment = async (req: Request, res: Response) => {
+  const { id } = req.query;
+
+  try {
+    const blogCommentInDB = await prisma.blog_comment.findUnique({
+      where: { id: id as string },
+    });
+
+    if (!blogCommentInDB) {
+      return errorHandler({
+        res,
+        customMessage: `blog comment with ID ${id} not found`,
+        customStatus: 404,
+      });
+    }
+
+    await prisma.blog_comment
+      .update({
+        where: { id: id as string },
+        data: { likes: { increment: 1 } },
+      })
+      .then((data) => {
+        return successHandler({
+          res,
+          message: `Success Increment Comment Like with ID ${id}`,
+        });
+      });
+  } catch (error) {}
+};
+
+export const dislikeBlogComment = async (req: Request, res: Response) => {
+  const { id } = req.query;
+
+  try {
+    const blogCommentInDB = await prisma.blog_comment.findUnique({
+      where: { id: id as string },
+    });
+
+    if (!blogCommentInDB) {
+      return errorHandler({
+        res,
+        customMessage: `blog comment with ID ${id} not found`,
+        customStatus: 404,
+      });
+    }
+
+    await prisma.blog_comment
+      .update({
+        where: { id: id as string },
+        data: { dislike: { increment: 1 } },
+      })
+      .then((data) => {
+        return successHandler({
+          res,
+          message: `Success Increment Comment Dislike with ID ${id}`,
+        });
+      });
+  } catch (error) {}
 };
