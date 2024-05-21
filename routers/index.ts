1d4852652bf6c6b257537aa7ee2c572e9ea57ea4
Dandy Candra <dandycandrapratama@gmail.com>
2024-05-21 13:30:51 +0700

    declare new route

--- routers/index.ts
+++ routers/index.ts
@@ -2,3 +2,4 @@ export { default as myProjectsRouter } from "./myprojects.routers";
 export { default as blogRouter } from "./blog.routers";
 export { default as commentBlogRouter } from "./commentBlog.routers";
 export { default as authRouter } from "./auth.routers";
+export { default as certificateRoute } from "./certificate.routers";
