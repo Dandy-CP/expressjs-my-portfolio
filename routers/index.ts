b0c4f4aecd48ff407b7e178352fcc4a3825e636d
Dandy Candra <dandycandrapratama@gmail.com>
2024-05-22 10:40:13 +0700

    add export index identity

--- routers/index.ts
+++ routers/index.ts
@@ -3,3 +3,4 @@ export { default as blogRouter } from "./blog.routers";
 export { default as commentBlogRouter } from "./commentBlog.routers";
 export { default as authRouter } from "./auth.routers";
 export { default as certificateRoute } from "./certificate.routers";
+export { default as identityRouter } from "./identity.routers";
