ca645d879c5cae69315949b4901a0a37b5dc0682
Dandy Candra <dandycandrapratama@gmail.com>
2024-06-04 14:15:48 +0700

    add session, redis connect and declare route 2FA

--- app.ts
+++ app.ts
@@ -1,12 +1,16 @@
 import express, { Request, Response, Application } from "express";
+import connectRedis from "connect-redis";
+import session from "express-session";
 import cors from "cors";
 import bodyParser from "body-parser";
 import helmet from "helmet";
 import dotenv from "dotenv";
+import redis from "@/config/redisClient";
 import prisma from "@/prisma/prisma";
 import validateEnv from "@/utils/validateEnv";
 import {
   authRouter,
+  twoFARoute,
   myProjectsRouter,
   certificateRoute,
   identityRouter,
@@ -16,6 +20,7 @@ import {
 
 dotenv.config();
 validateEnv();
+redis.connect();
 
 const app: Application = express();
 const PORT = process.env.PORT || 8080;
@@ -30,18 +35,31 @@ app.use(
 app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+  session({
+    store: new connectRedis({ client: redis }),
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 240 * 60 * 60 * 1000,
+    },
+  })
+);
 
 app.get("/", async (req: Request, res: Response) => {
   try {
     const checkDatabase = await prisma.$queryRaw`SELECT 1`;
     const statusDatabase = checkDatabase ? "Running" : "Error";
+    const statusRedis = redis.isReady;
 
     res.status(200).json({
       message: "Welcome To My Portfolio Rest API",
       database: statusDatabase,
+      redis: statusRedis,
     });
   } catch (error) {
-    res.status(200).json({
+    res.status(500).json({
       message: "Welcome To My Portfolio Rest API",
       database: "Error",
       error: error.message,
@@ -50,6 +68,7 @@ app.get("/", async (req: Request, res: Response) => {
 });
 
 authRouter(app);
+twoFARoute(app);
 myProjectsRouter(app);
 certificateRoute(app);
 identityRouter(app);
@@ -61,6 +80,14 @@ app.use((req: Request, res: Response) => {
     message: `Route ${req.path} Not Found`,
     status: 404,
   });
+});
+
+redis.on("error", (error) => {
+  console.error(`[Redis]: Opps Something Wrong ${error.message}`);
+});
+
+redis.on("connect", () => {
+  console.log("[Redis]: Connected to redis successfully");
 });
 
 app
