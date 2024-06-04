4a90e9f2fb4c7842d77599d34288143fa3938983
Dandy Candra <dandycandrapratama@gmail.com>
2024-06-04 14:19:03 +0700

    adjust authGuard to check session and token expired

--- middleware/authGuard.ts
+++ middleware/authGuard.ts
@@ -1,10 +1,29 @@
 import { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
+import redis from "@/config/redisClient";
 import { errorHandler } from "@/middleware/responseHandler";
-import { decodeTypeJWT } from "@/types/auth";
 
-const authGuard = (req: Request, res: Response, next: NextFunction) => {
+const authGuard = async (req: Request, res: Response, next: NextFunction) => {
   const authHeader = req.headers.authorization;
+  const session = req.session;
+
+  const token = authHeader?.split(" ")[1];
+  const sessionValue = await redis.get(`sess:${session.id}`);
+
+  const isTokenExpired = (token: string) => {
+    const arrayToken = token.split(".");
+    const tokenPayload = JSON.parse(atob(arrayToken[1]));
+
+    return Math.floor(new Date().getTime() / 1000) >= tokenPayload?.exp;
+  };
+
+  const isSessionExpired = (sessionValue: string) => {
+    const valueJSON = JSON.parse(sessionValue);
+
+    return (
+      new Date().getTime() >= new Date(valueJSON?.cookie?.expires).getTime()
+    );
+  };
 
   if (!authHeader) {
     return res.status(403).json({
@@ -12,7 +31,23 @@ const authGuard = (req: Request, res: Response, next: NextFunction) => {
     });
   }
 
-  const token = authHeader.split(" ")[1];
+  if (!sessionValue) {
+    return res.status(403).json({
+      message: "Your not authenticated, please Login",
+    });
+  }
+
+  if (isTokenExpired(token)) {
+    return res.status(403).json({
+      message: "Token Is Expired",
+    });
+  }
+
+  if (isSessionExpired(sessionValue)) {
+    return res.status(403).json({
+      message: "Session Is Expired",
+    });
+  }
 
   jwt.verify(
     token,
@@ -23,7 +58,6 @@ const authGuard = (req: Request, res: Response, next: NextFunction) => {
         return errorHandler({ res, error });
       }
 
-      req.user = decode as decodeTypeJWT;
       return next();
     }
   );
