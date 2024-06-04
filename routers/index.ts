e6550be9043ae6d6331f684e830338756a1da269
Dandy Candra <dandycandrapratama@gmail.com>
2024-06-04 14:20:21 +0700

    add 2FA route to export index

--- routers/index.ts
+++ routers/index.ts
@@ -4,3 +4,4 @@ export { default as commentBlogRouter } from "./commentBlog.routers";
 export { default as authRouter } from "./auth.routers";
 export { default as certificateRoute } from "./certificate.routers";
 export { default as identityRouter } from "./identity.routers";
+export { default as twoFARoute } from "./2FA.routers";
